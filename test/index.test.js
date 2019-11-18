ee6eea780b66a8a9b94f8d0c933d912ba7be4c3e
Rick Chang <yuningc0325@gmail.com>
2019-11-18 22:44:00 +0800

    CI test

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,9 @@ const reply = require('../controller/reply/index.js')
 
 /* eslint-env jest */
 const { textReply } = reply
-test('reply type check', () => {
+test('reply type check1', () => {
   expect(textReply('123')).toBeType('string')
+})
+test('reply type check2', () => {
+  expect(textReply('1234')).toBeType('string')
 })
